a4f4c47faff3a177bca121c50d3c5dfff74402f7
amargue <antoniojesus.marchena@iesdonana.org>
2023-11-03 22:31:48 +0100

    crea migraciones

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,21 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('nickname', 30);
+            $table->string('name', 50);
+            $table->string('surnames', 50);
+            $table->string('password', 255);
+            $table->date('fecha_nacimiento');
+            $table->bigInteger('country_id')->nullable()->unsigned();
+            $table->string('email', 255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->bigInteger('rol_id')->unsigned()->default(3);
+            $table->boolean('blocked')->default(false);
+            $table->integer('strikes')->unsigned()->default(0);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries');
         });
     }
 
